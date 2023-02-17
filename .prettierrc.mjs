d92ad078fc9a73d02afd3a280abaabcf93d05544
zhaogongchengsi <zzh1586169624@163.com>
2023-02-17 14:52:56 +0800

    style: code style

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,17 +1,19 @@
 export default {
-  printWidth: 100, //单行长度
+  printWidth: 80, //单行长度
   tabWidth: 2, //缩进长度
   useTabs: false, //使用空格代替tab缩进
   semi: true, //句末使用分号
   singleQuote: true, //使用单引号
   quoteProps: "as-needed", //仅在必需时为对象的key添加引号
   trailingComma: "all", //多行时尽可能打印尾随逗号
-  bracketSpacing: true, //在对象前后添加空格-eg: { foo: bar }
+  bracketSpacing: false, //在对象前后添加空格-eg: { foo: bar }
   requirePragma: false, //无需顶部注释即可格式化
   insertPragma: false, //在已被preitter格式化的文件顶部加上标注
   proseWrap: "preserve", //不知道怎么翻译
   htmlWhitespaceSensitivity: "ignore", //对HTML全局空白不敏感
-  vueIndentScriptAndStyle: false, //不对vue中的script及style标签缩进
+  singleAttributePerLine: false, //多个属性时每个属性独占一行
+  vueIndentScriptAndStyle: true, //不对vue中的script及style标签缩进
+  bracketSameLine: false, //将>多行元素的'>'折叠到最后一行的末尾
   endOfLine: "lf", //结束行形式
   embeddedLanguageFormatting: "auto", //对引用代码进行格式化
 };
--- src/layouts/ComponNotExist/index.vue
+++ src/layouts/ComponNotExist/index.vue
@@ -9,15 +9,12 @@
       </div>
       <div class="flex items-center text-xl font-sans font-semibold space-x-20">
         <code
-          class="select-all px-3 py-0.5 border-rd-2 bg-gray-400 text-gray-50 dark:bg-gray-900 border border-gray-400 truncate max-w-150">
-              {{ props.componentPath }}
-            </code>
+          class="select-all px-3 py-0.5 border-rd-2 bg-gray-400 text-gray-50 dark:bg-gray-900 border border-gray-400 truncate max-w-150">{{ props.componentPath }}</code>
         <ul
           class="max-w-100 max-h-100 overflow-y-auto app-scrollbar divide-y-1 divide-Gray-200 divide-opacity-20 dark:divide-opacity-5">
           <li class="truncate select-all leading-relaxed" v-for="item in props.modules">{{ item }}</li>
         </ul>
       </div>
-
       <div>
         <router-view></router-view>
       </div>
