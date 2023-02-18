b05165677e16b3731b9aa94fe7a883b927c5c40a
zhaogongchengsi <zzh1586169624@163.com>
2023-02-18 16:20:21 +0800

    change

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,12 @@ import components from "unplugin-vue-components/vite";
 import { ArcoResolver } from "unplugin-vue-components/resolvers";
 import { viteMockServe } from "vite-plugin-mock";
 
-import { pageGenerateRouter } from "vite-plugin-pages-generate-router";
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const dark = "class";
 
   const proxyprefix = env.VITE_PROXY;
   const proxytraget = env.VITE_TARGET;
-  const routermode = env.ROYTING_MODE;
   const usemock = env.VITE_USE_MOCK === "true" ? true : false;
 
   return {
@@ -41,10 +38,6 @@ export default defineConfig(({ mode }) => {
           mockPath: "mock", //mock文件路径，在根路径下创建一个mock文件
           prodEnabled: false, //生产环境下为false，这样就不会被打包到生产包中
           ignore: /^\_/, //忽略开始_路径
-        }),
-      routermode === "static" &&
-        pageGenerateRouter({
-          generateDir: "./src/views",
         }),
     ],
     resolve: {
